86d9a0d339eef703eba44eebe4646555858f085e
ZZ-5 <ivannikov.pavel.1994@gmail.com>
2024-04-05 19:54:50 +0300

    helpers

--- src/functions/helpers.ts
+++ src/functions/helpers.ts
@@ -4,46 +4,102 @@ export const calculations = (value: string, result: string) => {
   let sign = '';
   const action = ['-', '+', '*', '/'];
 
-  for (let i = value.length; i >= 0; i--) {
-    if (action.includes(value[i])) {
-      if (result === '') {
-        sign = value[i];
-        a = value.slice(0, i);
-        b = value.slice(i + 1);
+  // for (let i = value.length; i >= 0; i--) {
+  //   if (action.includes(value[i])) {
+  //     if (result === '') {
+  //       sign = value[i];
+  //       a = value.slice(0, i);
+  //       b = value.slice(i + 1);
+  //     } else {
+  //       sign = value[i];
+  //       a = result;
+  //       b = value.slice(i + 1);
+  //       break;
+  //     }
+  //   }
+  // }
+
+  let arr = [];
+  let str = '';
+
+  for (let i = 0; i < value.length; i++) {
+    if (!action.includes(value[i])) {
+      if (i === value.length - 1) {
+        str += value[i];
+        arr.push(+str);
+      } else if (!action.includes(value[i + 1])) {
+        str += value[i];
       } else {
-        sign = value[i];
-        a = result;
-        b = value.slice(i + 1);
-        break;
+        str += value[i];
+        arr.push(+str);
+        str = '';
       }
+    } else {
+      arr.push(value[i]);
     }
   }
 
-  switch (sign) {
-    case '+':
-      a = +a + +b;
-      a = a.toString();
-      result = a;
-      b = '';
-      break;
-    case '-':
-      a = +a - +b;
-      a = a.toString();
-      result = a;
-      b = '';
-      break;
-    case '*':
-      a = +a * +b;
-      a = a.toString();
-      result = a;
-      b = '';
-      break;
-    case '/':
-      a = +a / +b;
-      a = a.toString();
-      result = a;
-      b = '';
-      break;
-  }
+  arr.map(i => {
+    if (a === '' && typeof i === 'number') {
+      a = i;
+    } else if (typeof i === 'string') {
+      sign = i;
+    } else if (a !== 0 && b === '') {
+      b = i;
+    }
+    // console.log(a, sign, b);
+    if (sign !== '') {
+      switch (sign) {
+        case '+':
+          // console.log(a, sign, b);
+
+          a = +a + +b;
+          // console.log(a, sign, b);
+
+          b = '';
+          break;
+        case '-':
+          a = +a - +b;
+          b = '';
+          break;
+        case '*':
+          a = +a * +b;
+          b = '';
+          break;
+        case '/':
+          a = +a / +b;
+          b = '';
+          break;
+      }
+    }
+    console.log((result = a.toString()));
+  });
+
+  // switch (sign) {
+  //   case '+':
+  //     a = +a + +b;
+  //     a = a.toString();
+  //     result = a;
+  //     b = '';
+  //     break;
+  //   case '-':
+  //     a = +a - +b;
+  //     a = a.toString();
+  //     result = a;
+  //     b = '';
+  //     break;
+  //   case '*':
+  //     a = +a * +b;
+  //     a = a.toString();
+  //     result = a.toString();
+  //     b = '';
+  //     break;
+  //   case '/':
+  //     a = +a / +b;
+  //     a = a.toString();
+  //     result = a;
+  //     b = '';
+  //     break;
+
   return result;
 };
